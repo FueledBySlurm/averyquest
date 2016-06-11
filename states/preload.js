27b02b622dd3601d4a92503e39c36e3bd06b4c83
Moggy <pkuehl6@gmail.com>
2016-06-11 11:15:26 -0600

    spikes, lava, and hoppy johnson, oh my

--- states/preload.js
+++ states/preload.js
@@ -2,15 +2,17 @@ var preload = function(game) {}
 
 preload.prototype = {
   preload: function(){
-		this.game.load.tilemap('level1', 'assets/test5.json', null, Phaser.Tilemap.TILED_JSON);
+		this.game.load.tilemap('level1', 'assets/level.json', null, Phaser.Tilemap.TILED_JSON);
 		this.game.load.image('tiles-2', 'assets/Tile.png');
 		this.game.load.image('GroundTile', 'assets/GroundTile.png');
+    this.game.load.image('LavaSpikes', 'assets/LavaSpikes.png');
 		this.game.load.spritesheet('dude', 'assets/CheadleSprite.png', 50, 60);
 		this.game.load.spritesheet('coorsCan', 'assets/ZoorsCan.png', 50, 100);
 		this.game.load.image('averyCoin', 'assets/AveryCoin.png');
 		this.game.load.image('starBig', 'assets/games/starstruck/star2.png');
 		this.game.load.image('background', 'assets/BG.png');
 		this.game.load.image('startScreen', 'assets/TitleScreen.png');
+
 	},
 	create: function(){
 		this.game.state.start("StartScreen");
